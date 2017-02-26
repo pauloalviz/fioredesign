284e59f3656a2652959a2b11f392f74a7f4b0640
Paulo Alvis <paulo@loyal3.com>
2017-02-25 19:43:51 -0500

    deleting

--- m2devbox-debug.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-ssh_port=$(docker port magento2devbox_web_c34677f3da19131a5906e3eebf672dc4 22)
-ssh_port=${ssh_port#*:}
-
-ssh -N -p $ssh_port -R 9000:localhost:9000 magento2@127.0.0.1
